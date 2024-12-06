1f706be32b2e29ede824ccc5ac038c121ff19dfb
nttoan-khiem <119110657+nttoan-khiem@users.noreply.github.com>
2024-12-06 23:32:54 +0700

    Fix optimize LCD display, Only change LCD when really neccessary, Add cycle reset LCD.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def transTime(time):
     second = int(time - hour*3600 - minus*60)
     return hour, minus, second
 
-pins.analog_write_pin(AnalogPin.P13, 0)
-pins.analog_write_pin(AnalogPin.P14, 0)
+pins.analog_write_pin(AnalogPin.P15, 0)
+pins.analog_write_pin(AnalogPin.P10, 0)
 
 timeOn = 3
 timeOn = EEPROM.readw(0) #read old value of Hold time AE
@@ -19,7 +19,7 @@ I2C_LCD1602.backlight_on()
 timeRemainSetup = 5
 motor = 0
 onSetup = 0
-
+change = 1
 timeTriger = timeOff
 
 def on_forever():
@@ -27,32 +27,92 @@ def on_forever():
     global timeOn, timeOff, motor, timeRemainSetup, onSetup, timeTriger
     if timeRemainSetup > 0: #setup on process
         if onSetup == 0: #Setup delay time
-            string = "Setup: TimeOn   "
-            I2C_LCD1602.show_string(string, 0, 0)
-            hourSet, minSet, secSet = transTime(timeOn)
-            string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
-            I2C_LCD1602.show_string(string, 0, 1)
+            if change: 
+                string = "Setup: TimeOn   "
+                I2C_LCD1602.show_string(string, 0, 0)
+                hourSet, minSet, secSet = transTime(timeOn)
+                string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
+                I2C_LCD1602.show_string(string, 0, 1)
+                change = 0;
+            else:
+                hourSet, minSet, secSet = transTime(timeOn)
+                if hourSet >= 10:
+                    I2C_LCD1602.show_number(hourSet, 6, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 6, 1)
+                    I2C_LCD1602.show_number(hourSet, 7, 1)
+                if minSet >= 10:
+                    I2C_LCD1602.show_number(minSet, 9, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 9, 1)
+                    I2C_LCD1602.show_number(hourSet, 10, 1)
+                if secSet >= 10:
+                    I2C_LCD1602.show_number(secSet, 12, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 12, 1)
+                    I2C_LCD1602.show_number(secSet, 13, 1)                
         elif onSetup == 1: #Setup hold time
-            string = "Setup: TimeOff  "
-            I2C_LCD1602.show_string(string, 0, 0)
-            hourSet, minSet, secSet = transTime(timeOff)
-            string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
-            I2C_LCD1602.show_string(string, 0, 1)
+            if change:
+                string = "Setup: TimeOff  "
+                I2C_LCD1602.show_string(string, 0, 0)
+                hourSet, minSet, secSet = transTime(timeOff)
+                string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
+                I2C_LCD1602.show_string(string, 0, 1)
+                change = 0;
+            else:
+                hourSet, minSet, secSet = transTime(timeOff)
+                if hourSet >= 10:
+                    I2C_LCD1602.show_number(hourSet, 6, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 6, 1)
+                    I2C_LCD1602.show_number(hourSet, 7, 1)
+                if minSet >= 10:
+                    I2C_LCD1602.show_number(minSet, 9, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 9, 1)
+                    I2C_LCD1602.show_number(hourSet, 10, 1)
+                if secSet >= 10:
+                    I2C_LCD1602.show_number(secSet, 12, 1)
+                else:
+                    I2C_LCD1602.show_number(0, 12, 1)
+                    I2C_LCD1602.show_number(secSet, 13, 1)
     else:
-        hourSet, minSet, secSet = transTime(timeTriger)
-        string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
-        I2C_LCD1602.show_string(string, 0, 0)
-        if(motor):
-            string = "-----Active-----"
+        if change:
+            hourSet, minSet, secSet = transTime(timeTriger)
+            string = "Time: %2dh%2dm%2ds " % (hourSet, minSet, secSet)
+            I2C_LCD1602.show_string(string, 0, 0)
+            if(motor):
+                string = "-----Active-----"
+            else:
+                string = "----Inactive----"
+            I2C_LCD1602.show_string(string, 0, 1)
+            change = 0;
         else:
-            string = "----Inactive----"
-        I2C_LCD1602.show_string(string, 0, 1)
+            hourSet, minSet, secSet = transTime(timeTriger)
+            if hourSet >= 10:
+                I2C_LCD1602.show_number(hourSet, 6, 1)
+            else:
+                I2C_LCD1602.show_number(0, 6, 1)
+                I2C_LCD1602.show_number(hourSet, 7, 1)
+            if minSet >= 10:
+                I2C_LCD1602.show_number(minSet, 9, 1)
+            else:
+                I2C_LCD1602.show_number(0, 9, 1)
+                I2C_LCD1602.show_number(hourSet, 10, 1)
+            if secSet >= 10:
+                I2C_LCD1602.show_number(secSet, 12, 1)
+            else:
+                I2C_LCD1602.show_number(0, 12, 1)
+                I2C_LCD1602.show_number(secSet, 13, 1) 
+    basic.pause(100)           
 basic.forever(on_forever)
 
 def on_forever2():
     global timeOn, timeOff, timeRemainSetup, onSetup
     if onSetup == 0:
         if input.button_is_pressed(Button.A):
+            if timeRemainSetup == 0:
+                change = 1
             timeRemainSetup = 5
             counter = 0
             while input.button_is_pressed(Button.A):
@@ -66,6 +126,8 @@ def on_forever2():
             if timeOn < 0:
                 timeOn = 0
         elif input.button_is_pressed(Button.B):
+            if timeRemainSetup == 0:
+                change = 1
             timeRemainSetup = 5
             counter = 0
             while input.button_is_pressed(Button.B):
@@ -79,6 +141,8 @@ def on_forever2():
             if timeOn < 0:
                 timeOn = 0
         elif input.pin_is_pressed(TouchPin.P0):
+            if timeRemainSetup == 0:
+                change = 1
             while input.pin_is_pressed(TouchPin.P0):
                 pass
             onSetup = 1
@@ -88,6 +152,8 @@ def on_forever2():
             basic.clear_screen()
     elif onSetup == 1: #On ESA
         if input.button_is_pressed(Button.A):
+            if timeRemainSetup == 0:
+                change = 1
             timeRemainSetup = 5
             counter = 0
             while input.button_is_pressed(Button.A):
@@ -99,6 +165,8 @@ def on_forever2():
                     basic.pause(200)
             timeOff += 2
         elif input.button_is_pressed(Button.B):
+            if timeRemainSetup == 0:
+                change = 1
             timeRemainSetup = 5
             counter = 0
             while input.button_is_pressed(Button.B):
@@ -112,6 +180,8 @@ def on_forever2():
             if timeOff < 0:
                 timeOff = 0
         elif input.pin_is_pressed(TouchPin.P0):
+            if timeRemainSetup == 0:
+                change = 1
             while input.pin_is_pressed(TouchPin.P0):
                 pass
             onSetup = 0
@@ -125,16 +195,19 @@ basic.forever(on_forever2)
 def on_forever5():
     global motor, timeOn, timeOff, timeTriger
     if motor == 1:
-        pins.analog_write_pin(AnalogPin.P13, 0)
-        pins.analog_write_pin(AnalogPin.P14, 550)
+        pins.analog_write_pin(AnalogPin.P15, 0)
+        pins.analog_write_pin(AnalogPin.P10, 550)
         if timeTriger == 0:
             timeTriger = timeOff
+            change = 1
             motor = 0
     else:
-        pins.analog_write_pin(AnalogPin.P13, 0)
-        pins.analog_write_pin(AnalogPin.P14, 0)
+        pins.analog_write_pin(AnalogPin.P15, 0)
+        pins.analog_write_pin(AnalogPin.P10,
+         0)
         if timeTriger == 0:
             timeTriger = timeOn
+            change = 1
             motor = 1
     basic.pause(100)
 basic.forever(on_forever5)
@@ -150,5 +223,16 @@ def on_forever6():
         timeRemainSetup -= 1
     else:
         timeRemainSetup = 0
+    if timeRemainSetup == 1:
+        change = 1
 basic.forever(on_forever6)
+
+def on_forever7(): #Cycle reset I2CLCD
+    basic.pause(5000)
+    I2C_LCD1602.clear()
+    I2C_LCD1602.off()
+    I2C_LCD1602.lcd_init(39)
+    I2C_LCD1602.on()
+    change = 1
+basic.forever(on_forever7)
 # For Display
--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ function transTime(time: number): number[] {
     return [hour, minus, second]
 }
 
-pins.analogWritePin(AnalogPin.P13, 0)
-pins.analogWritePin(AnalogPin.P14, 0)
+pins.analogWritePin(AnalogPin.P15, 0)
+pins.analogWritePin(AnalogPin.P10, 0)
 let timeOn = 3
 timeOn = EEPROM.readw(0)
 // read old value of Hold time AE
@@ -18,30 +18,86 @@ I2C_LCD1602.BacklightOn()
 let timeRemainSetup = 5
 let motor = 0
 let onSetup = 0
+let change = 1
 let timeTriger = timeOff
 basic.forever(function on_forever() {
     let string: string;
+    let change: number;
     // ===========================================================================
     
     if (timeRemainSetup > 0) {
         // setup on process
         if (onSetup == 0) {
             // Setup delay time
-            string = "Setup: TimeOn   "
-            I2C_LCD1602.ShowString(string, 0, 0)
-            let [hourSet, minSet, secSet] = transTime(timeOn)
-            string = `Time: ${hourSet}h${minSet}m${secSet}s `
-            I2C_LCD1602.ShowString(string, 0, 1)
+            if (change) {
+                string = "Setup: TimeOn   "
+                I2C_LCD1602.ShowString(string, 0, 0)
+                let [hourSet, minSet, secSet] = transTime(timeOn)
+                string = `Time: ${hourSet}h${minSet}m${secSet}s `
+                I2C_LCD1602.ShowString(string, 0, 1)
+                change = 0
+            } else {
+                let [hourSet, minSet, secSet] = transTime(timeOn)
+                if (hourSet >= 10) {
+                    I2C_LCD1602.ShowNumber(hourSet, 6, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 6, 1)
+                    I2C_LCD1602.ShowNumber(hourSet, 7, 1)
+                }
+                
+                if (minSet >= 10) {
+                    I2C_LCD1602.ShowNumber(minSet, 9, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 9, 1)
+                    I2C_LCD1602.ShowNumber(hourSet, 10, 1)
+                }
+                
+                if (secSet >= 10) {
+                    I2C_LCD1602.ShowNumber(secSet, 12, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 12, 1)
+                    I2C_LCD1602.ShowNumber(secSet, 13, 1)
+                }
+                
+            }
+            
         } else if (onSetup == 1) {
             // Setup hold time
-            string = "Setup: TimeOff  "
-            I2C_LCD1602.ShowString(string, 0, 0)
-            let [hourSet, minSet, secSet] = transTime(timeOff)
-            string = `Time: ${hourSet}h${minSet}m${secSet}s `
-            I2C_LCD1602.ShowString(string, 0, 1)
+            if (change) {
+                string = "Setup: TimeOff  "
+                I2C_LCD1602.ShowString(string, 0, 0)
+                let [hourSet, minSet, secSet] = transTime(timeOff)
+                string = `Time: ${hourSet}h${minSet}m${secSet}s `
+                I2C_LCD1602.ShowString(string, 0, 1)
+                change = 0
+            } else {
+                let [hourSet, minSet, secSet] = transTime(timeOff)
+                if (hourSet >= 10) {
+                    I2C_LCD1602.ShowNumber(hourSet, 6, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 6, 1)
+                    I2C_LCD1602.ShowNumber(hourSet, 7, 1)
+                }
+                
+                if (minSet >= 10) {
+                    I2C_LCD1602.ShowNumber(minSet, 9, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 9, 1)
+                    I2C_LCD1602.ShowNumber(hourSet, 10, 1)
+                }
+                
+                if (secSet >= 10) {
+                    I2C_LCD1602.ShowNumber(secSet, 12, 1)
+                } else {
+                    I2C_LCD1602.ShowNumber(0, 12, 1)
+                    I2C_LCD1602.ShowNumber(secSet, 13, 1)
+                }
+                
+            }
+            
         }
         
-    } else {
+    } else if (change) {
         let [hourSet, minSet, secSet] = transTime(timeTriger)
         string = `Time: ${hourSet}h${minSet}m${secSet}s `
         I2C_LCD1602.ShowString(string, 0, 0)
@@ -52,14 +108,44 @@ basic.forever(function on_forever() {
         }
         
         I2C_LCD1602.ShowString(string, 0, 1)
+        change = 0
+    } else {
+        let [hourSet, minSet, secSet] = transTime(timeTriger)
+        if (hourSet >= 10) {
+            I2C_LCD1602.ShowNumber(hourSet, 6, 1)
+        } else {
+            I2C_LCD1602.ShowNumber(0, 6, 1)
+            I2C_LCD1602.ShowNumber(hourSet, 7, 1)
+        }
+        
+        if (minSet >= 10) {
+            I2C_LCD1602.ShowNumber(minSet, 9, 1)
+        } else {
+            I2C_LCD1602.ShowNumber(0, 9, 1)
+            I2C_LCD1602.ShowNumber(hourSet, 10, 1)
+        }
+        
+        if (secSet >= 10) {
+            I2C_LCD1602.ShowNumber(secSet, 12, 1)
+        } else {
+            I2C_LCD1602.ShowNumber(0, 12, 1)
+            I2C_LCD1602.ShowNumber(secSet, 13, 1)
+        }
+        
     }
     
+    basic.pause(100)
 })
 basic.forever(function on_forever2() {
+    let change: number;
     let counter: number;
     
     if (onSetup == 0) {
         if (input.buttonIsPressed(Button.A)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             timeRemainSetup = 5
             counter = 0
             while (input.buttonIsPressed(Button.A)) {
@@ -78,6 +164,10 @@ basic.forever(function on_forever2() {
             }
             
         } else if (input.buttonIsPressed(Button.B)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             timeRemainSetup = 5
             counter = 0
             while (input.buttonIsPressed(Button.B)) {
@@ -96,6 +186,10 @@ basic.forever(function on_forever2() {
             }
             
         } else if (input.pinIsPressed(TouchPin.P0)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             while (input.pinIsPressed(TouchPin.P0)) {
                 
             }
@@ -109,6 +203,10 @@ basic.forever(function on_forever2() {
     } else if (onSetup == 1) {
         // On ESA
         if (input.buttonIsPressed(Button.A)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             timeRemainSetup = 5
             counter = 0
             while (input.buttonIsPressed(Button.A)) {
@@ -123,6 +221,10 @@ basic.forever(function on_forever2() {
             }
             timeOff += 2
         } else if (input.buttonIsPressed(Button.B)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             timeRemainSetup = 5
             counter = 0
             while (input.buttonIsPressed(Button.B)) {
@@ -141,6 +243,10 @@ basic.forever(function on_forever2() {
             }
             
         } else if (input.pinIsPressed(TouchPin.P0)) {
+            if (timeRemainSetup == 0) {
+                change = 1
+            }
+            
             while (input.pinIsPressed(TouchPin.P0)) {
                 
             }
@@ -156,20 +262,23 @@ basic.forever(function on_forever2() {
     basic.pause(100)
 })
 basic.forever(function on_forever5() {
+    let change: number;
     
     if (motor == 1) {
-        pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P14, 550)
+        pins.analogWritePin(AnalogPin.P15, 0)
+        pins.analogWritePin(AnalogPin.P10, 550)
         if (timeTriger == 0) {
             timeTriger = timeOff
+            change = 1
             motor = 0
         }
         
     } else {
-        pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P14, 0)
+        pins.analogWritePin(AnalogPin.P15, 0)
+        pins.analogWritePin(AnalogPin.P10, 0)
         if (timeTriger == 0) {
             timeTriger = timeOn
+            change = 1
             motor = 1
         }
         
@@ -178,6 +287,7 @@ basic.forever(function on_forever5() {
     basic.pause(100)
 })
 basic.forever(function on_forever6() {
+    let change: number;
     
     basic.pause(1000)
     if (timeTriger > 0) {
@@ -192,4 +302,17 @@ basic.forever(function on_forever6() {
         timeRemainSetup = 0
     }
     
+    if (timeRemainSetup == 1) {
+        change = 1
+    }
+    
+})
+basic.forever(function on_forever7() {
+    // Cycle reset I2CLCD
+    basic.pause(5000)
+    I2C_LCD1602.clear()
+    I2C_LCD1602.off()
+    I2C_LCD1602.LcdInit(39)
+    I2C_LCD1602.on()
+    let change = 1
 })
